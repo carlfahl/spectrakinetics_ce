bf85ef329796dc80ec3e84f82d7765b9a601add3
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2022-06-28 15:41:45 -0600

    updates

--- spectraKinetics_webapp.py
+++ spectraKinetics_webapp.py
@@ -4,9 +4,11 @@
 # Copyright 2016 
 #
 
-from flask import Flask, render_templete
+import os
+from flask import Flask, render_templete, request, redirect, url_for
 from flask.ext.wtf import Form
 from wtforms.fields import StringField, SubmitField
+from werkzeug.utils import secure_filename
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
@@ -31,9 +33,43 @@ class dataPoint (db.Model):
 	xData = db.Column(db.Float)
 	yData = db.Column(db.Float)
 
+class 
+
 class FileForm(Form):
 	'''
 	'''
 
 	filename = StringField()
 	fileContents = 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=[])
+def index():
+	'''
+	'''
+
+
+@app.route('/fileup', methods=['GET', 'POST'])
+def upload_file():
+    '''
+    '''
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
